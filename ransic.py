c9fb9587beeb223e71cfcfd210ecad780752bdc2
SGtOriginal <sgtoriginal@gmail.com>
2023-01-15 19:25:15 +0530

    Made some monkey fixes to this

--- ransic.py
+++ ransic.py
@@ -13,20 +13,26 @@ def rand_num(min: int, max: int):
     number = secretsGenerator.randint(min, max)
     return number
 
+def ogg_conv(file):
+    return f"{file.rsplit('.', 1)[0]}.ogg"
+
+def fixer():
+    files = os.listdir(os.path.join(os.getcwd(), 'sound-files'))
+    for file in files:
+        # print(f"ffmpeg.exe -y -loglevel quiet -i '{os.path.join(os.path.join(os.getcwd(), 'sound-files'), file)}' '{os.path.join(os.path.join(os.getcwd(), 'sound-files'), ogg_conv(file))}'")
+        os.system(f"powershell -c ./ffmpeg.exe -y -loglevel quiet -i '{os.path.join(os.path.join(os.getcwd(), 'sound-files'), file)}' '{os.path.join(os.path.join(os.getcwd(), 'sound-files'), ogg_conv(file))}'")
+        if file.rsplit('.', 1)[1] != "ogg":
+            os.remove(f"sound-files/{file}")
+
 def sound_files():
     audio_list = []
     
-    mp3_files = glob.iglob('sound-files/*.mp3', recursive=True)
-    for mp3 in mp3_files:
-        root_dir = os.path.abspath(os.path.dirname(__file__))
-        bg_path = os.path.join(root_dir, mp3)
-        audio_list.append(bg_path)
+    fixer()
     
-    wav_files = glob.iglob('sound-files/*.wav', recursive=True)
-    for wav in wav_files:
-        root_dir = os.path.abspath(os.path.dirname(__file__))
-        bg_path = os.path.join(root_dir, wav)
-        audio_list.append(bg_path)
+    files = os.listdir(os.path.join(os.getcwd(), 'sound-files'))
+    for file in files:
+        path = os.path.join(os.path.join(os.getcwd(), 'sound-files'), file)
+        audio_list.append(path)
     
     return audio_list
 
@@ -40,15 +46,44 @@ def main():
         
         song_path = audio_list[song_num]
         
-        song_start_pos = rand_num(min=0, max=(int((audioread.audio_open(song_path).duration))))
+        print(song_path)
         
-        song_end_pos = rand_num(min=0, max=song_start_pos)
+        if round(audioread.audio_open(song_path).duration) <= 2:
+            mixer.music.load(song_path)
+            
+            mixer.music.play()
+            
+            time.sleep(audioread.audio_open(song_path).duration)
         
-        mixer.music.load(song_path)
-        
-        mixer.music.play(start=song_start_pos)
-        
-        time.sleep(song_end_pos)
+        else:
+            song_start_pos = rand_num(min=2, max=(round(audioread.audio_open(song_path).duration)))
+
+            #Monkey Fix
+            if song_start_pos == 2:
+                song_start_pos = 3
+
+            song_end_pos = rand_num(min=1, max=((song_start_pos)-1))
+            
+            mixer.music.load(song_path)
+
+            monkey_fix = True
+
+            while monkey_fix:
+                try:
+                    mixer.music.play(start=song_start_pos)
+                except Exception as e:
+                    song_start_pos = rand_num(min=2, max=(round(audioread.audio_open(song_path).duration)))
+
+                    if song_start_pos == 2:
+                        song_start_pos = 3
+
+                    song_end_pos = rand_num(min=1, max=((song_start_pos) - 1))
+
+                    mixer.music.load(song_path)
+                else:
+                    monkey_fix = False
+
+            time.sleep(song_end_pos)
         
         mixer.music.stop()
 
